9ab02cba2ffebd5ac790e3553a87d4c28d49f297
darthjee <darthjee@gmail.com>
2020-04-12 17:11:24 +0200

    Add migration

--- source/db/schema.rb
+++ source/db/schema.rb
@@ -10,6 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_04_12_094537) do
+
+  create_table "simulations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
