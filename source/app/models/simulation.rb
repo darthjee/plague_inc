5e1ef4616da5c087865e467c53dce0768769a08e
darthjee <darthjee@gmail.com>
2020-04-16 19:58:48 +0200

    Make name mandatory

--- source/app/models/simulation.rb
+++ source/app/models/simulation.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Simulation < ApplicationRecord
-  validates_presence_of :algorithm
+  validates_presence_of :name, :algorithm
 end
--- source/db/migrate/20200416172306_add_simulation_fields.rb
+++ source/db/migrate/20200416172306_add_simulation_fields.rb
@@ -1,5 +1,8 @@
 class AddSimulationFields < ActiveRecord::Migration[5.2]
   def change
+    change_column :simulations, :name, :string, null: false, default: "UNAMED"
+    change_column :simulations, :name, :string, null: false, default: nil
+
     change_table :simulations do |t|
       t.string :algorithm, size: 10, null: false, default: :infection
     end
