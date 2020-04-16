b6fea07fe4a39e482fac781b27913bb02d478fa8
darthjee <darthjee@gmail.com>
2020-04-16 22:23:24 +0200

    Add to the form

--- source/db/migrate/20200416172306_add_simulation_fields.rb
+++ source/db/migrate/20200416172306_add_simulation_fields.rb
@@ -4,7 +4,7 @@ class AddSimulationFields < ActiveRecord::Migration[5.2]
     change_column :simulations, :name, :string, null: false, default: nil
 
     change_table :simulations do |t|
-      t.string :algorithm, size: 10, null: false, default: :infection
+      t.string :algorithm, size: 10, null: false, default: :contagion
     end
     change_column :simulations, :algorithm, :string, size: 10, null: false, default: nil
   end
