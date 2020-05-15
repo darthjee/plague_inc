7064eae3c11ebd45ee84055ba4d294000fcfacc4
darthjee <darthjee@gmail.com>
2020-05-15 18:45:56 +0200

    Fix decorator spec

--- source/spec/models/simulation/decorator_spec.rb
+++ source/spec/models/simulation/decorator_spec.rb
@@ -5,17 +5,18 @@ require 'spec_helper'
 describe Simulation::Decorator do
   subject(:decorator) { described_class.new(object) }
 
-  let(:attributes) { %w[id name algorithm] }
+  let(:attributes)     { %w[id name algorithm] }
+  let(:decorator_json) { JSON.parse(decorator.to_json) }
 
   describe '#to_json' do
     context 'when object is one entity' do
       let(:object) { create(:simulation) }
       let(:expected_json) do
-        object.as_json.slice(*attributes).to_json
+        object.as_json.slice(*attributes).as_json
       end
 
       it 'returns expected json' do
-        expect(decorator.to_json).to eq(expected_json)
+        expect(decorator_json).to eq(expected_json)
       end
 
       context 'when object is invalid but object has not been validated' do
@@ -24,7 +25,7 @@ describe Simulation::Decorator do
         end
 
         it 'returns expected json without errors' do
-          expect(decorator.to_json).to eq(expected_json)
+          expect(decorator_json).to eq(expected_json)
         end
       end
 
@@ -44,13 +45,14 @@ describe Simulation::Decorator do
           object
             .as_json
             .slice(*attributes)
-            .merge(errors: expected_errors).to_json
+            .merge(errors: expected_errors)
+            .deep_stringify_keys
         end
 
         before { object.valid? }
 
         it 'returns expected json with errors' do
-          expect(decorator.to_json).to eq(expected_json)
+          expect(decorator_json).to eq(expected_json)
         end
       end
     end
@@ -61,18 +63,18 @@ describe Simulation::Decorator do
       let(:expected_json) do
         object.map do |simulation|
           simulation.as_json.slice(*attributes)
-        end.to_json
+        end.as_json
       end
 
       it 'returns expected json' do
-        expect(decorator.to_json).to eq(expected_json)
+        expect(decorator_json).to eq(expected_json)
       end
 
       context 'when object is a collection of invalid not validated objects' do
         let(:object) { build_list(:simulation, 3, name: nil, algorithm: nil) }
 
         it 'returns expected json without errors' do
-          expect(decorator.to_json).to eq(expected_json)
+          expect(decorator_json).to eq(expected_json)
         end
       end
 
@@ -97,11 +99,11 @@ describe Simulation::Decorator do
               .as_json
               .slice(*attributes)
               .merge(errors: expected_errors)
-          end.to_json
+          end.as_json
         end
 
         it 'returns expected json' do
-          expect(decorator.to_json).to eq(expected_json)
+          expect(decorator_json).to eq(expected_json)
         end
       end
     end
