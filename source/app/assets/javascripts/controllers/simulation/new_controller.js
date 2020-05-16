38bb185b57b7fd39716387dfedb471835b3d6182
darthjee <darthjee@gmail.com>
2020-05-16 12:31:07 +0200

    Add simulation controller

---
+++ source/app/assets/javascripts/controllers/simulation/new_controller.js
@@ -0,0 +1,21 @@
+(function(_, angular, Cyberhawk) {
+  var app = angular.module('simulation/new_controller', [
+    'cyberhawk/controller',
+    'cyberhawk/notifier',
+  ]);
+
+
+  function Controller(builder, notifier, $location) {
+    this.construct(builder.build($location), notifier, $location);
+  }
+
+  var fn = Controller.prototype;
+
+  _.extend(fn, Cyberhawk.Controller.prototype);
+
+  app.controller('Simulation.NewController', [
+    'cyberhawk_requester', 'cyberhawk_notifier', '$location',
+    Controller
+  ]);
+}(window._, window.angular, window.Cyberhawk));
+
