1401b030429ee05c7e1a52d39230354c799ac90c
darthjee <darthjee@gmail.com>
2020-05-18 16:44:53 +0200

    Add form object

--- source/app/helpers/form_helper.rb
+++ source/app/helpers/form_helper.rb
@@ -4,28 +4,10 @@ module FormHelper
   def bootstrap_input(model, field,
                       placeholder: nil,
                       label: field.to_s.capitalize.gsub(/_/, ' '))
-    locals = {
-      label: label,
-      ng_errors: [model, :errors, field].join('.'),
-      ng_model: [model, field].join('.'),
-      model: model,
-      field: field,
-      placeholder: placeholder
-    }
-
-    render partial: 'templates/forms/input', locals: locals
+    Form.new(self).bootstrap_input(model, field, placeholder: placeholder, label: label)
   end
 
   def bootstrap_select(model, field, label: field.capitalize, options: [])
-    locals = {
-      label: label,
-      ng_errors: [model, :errors, field].join('.'),
-      ng_model: [model, field].join('.'),
-      model: model,
-      field: field,
-      options: options
-    }
-
-    render partial: 'templates/forms/select', locals: locals
+    Form.new(self).bootstrap_select(model, field, label: label, options: options)
   end
 end
---
+++ source/app/helpers/form_helper/form.rb
@@ -0,0 +1,43 @@
+module FormHelper
+  class Form
+    def initialize(renderer)
+      @renderer = renderer
+    end
+
+    def bootstrap_input(model, field,
+                        placeholder: nil,
+                        label: field.to_s.capitalize.gsub(/_/, ' '))
+      Form.new(self)
+      locals = {
+        label: label,
+        ng_errors: [model, :errors, field].join('.'),
+        ng_model: [model, field].join('.'),
+        model: model,
+        field: field,
+        placeholder: placeholder
+      }
+
+      render partial: 'templates/forms/input', locals: locals
+    end
+
+    def bootstrap_select(model, field, label: field.capitalize, options: [])
+      locals = {
+        label: label,
+        ng_errors: [model, :errors, field].join('.'),
+        ng_model: [model, field].join('.'),
+        model: model,
+        field: field,
+        options: options
+      }
+
+      render partial: 'templates/forms/select', locals: locals
+    end
+
+    private
+
+    attr_reader :renderer
+
+    delegate :render, to: :renderer
+  end
+end
+
