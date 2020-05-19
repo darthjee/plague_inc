62f2e4bbb04610b272fe1fd58ac120531f257c58
darthjee <darthjee@gmail.com>
2020-05-19 14:58:44 +0200

    Add down migration

--- source/db/migrate/20200519100617_add_size_to_groups.rb
+++ source/db/migrate/20200519100617_add_size_to_groups.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 class AddSizeToGroups < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_table :simulation_contagion_groups do |t|
       t.integer :size, null: false, default: 100
       t.change :size, :integer, null: false
+    end
+  end
+
+  def down
+    change_table :simulation_contagion_groups do |t|
+      t.remove :size
     end
   end
 end
