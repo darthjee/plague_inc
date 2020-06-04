7b691432ee43d6d50e15f3cac5336b2d752f6081
darthjee <darthjee@gmail.com>
2020-06-04 23:14:26 +0200

    Add behavior model

---
+++ source/app/models/simulation/contagion/behavior.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Simulation < ApplicationRecord
+  class Contagion < ApplicationRecord
+    class Behavior < ApplicationRecord
+      ALLOWED_ATTRIBUTES = %i[name size].freeze
+
+      belongs_to :contagion
+
+      validates_presence_of :contagion
+      validates :interactions,
+                presence: true,
+                numericality: { greater_than_or_equal_to: 0 }
+      validates :contagion_risk,
+                presence: true,
+                inclusion: { in: (0.0..1.0) }
+    end
+  end
+end
+
