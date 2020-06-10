05ab7d04cf97b6abb203034a6191fe7f84b189eb
darthjee <darthjee@gmail.com>
2020-06-10 21:52:39 +0200

    Add reference to factories

--- source/spec/support/factories/simulation/contagion/behavior.rb
+++ source/spec/support/factories/simulation/contagion/behavior.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :contagion_behavior, class: 'Simulation::Contagion::Behavior' do
     interactions   { 15 }
     contagion_risk { 0.5 }
+    reference      { SecureRandom.hex(5) }
     contagion
   end
 end
