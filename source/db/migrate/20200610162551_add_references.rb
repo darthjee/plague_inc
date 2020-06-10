85efc3cbf78c1fe19c0c1ac0322118249aaca54e
darthjee <darthjee@gmail.com>
2020-06-10 18:43:01 +0200

    Add references

---
+++ source/db/migrate/20200610162551_add_references.rb
@@ -0,0 +1,13 @@
+class AddReferences < ActiveRecord::Migration[5.2]
+  def change
+    change_table :simulation_contagion_groups do |t|
+      t.string :reference, size: 10
+      t.index [:reference, :contagion_id], unique: true
+    end
+
+    change_table :simulation_contagion_behaviors do |t|
+      t.string :reference, size: 10
+      t.index [:reference, :contagion_id], unique: true, name: :contagion_behaviors_reference_contagion_id
+    end
+  end
+end
