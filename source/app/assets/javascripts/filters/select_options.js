838d2a1b68a9ba0722cb8cc537ae771246cd963f
darthjee <darthjee@gmail.com>
2020-07-01 21:51:13 +0200

    Split filter

--- source/app/assets/javascripts/filters/select_options.js
+++ source/app/assets/javascripts/filters/select_options.js
@@ -2,10 +2,20 @@
   angular
     .module('plague_inc')
     .filter('select_transformer', function() {
-      return function(input, mappings, key, field) {
+      return function(input, mappings, key) {
         return _.find(mappings, function(object) {
           return object[key] == input
-        })[field]
+        })
+      };
+    });
+
+  angular
+    .module('plague_inc')
+    .filter('dig', function() {
+      return function(input, keys) {
+        return _.reduce(keys.split('.'), function(result, key) {
+          return result[key];
+        }, input)
       };
     });
 }(window.angular));
