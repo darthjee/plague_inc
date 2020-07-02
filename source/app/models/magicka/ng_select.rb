51e9a34114af485e5736b38361685bff2c412179
darthjee <darthjee@gmail.com>
2020-07-02 11:39:09 +0200

    Rubocop fix

--- source/app/models/magicka/ng_select.rb
+++ source/app/models/magicka/ng_select.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-class Magicka::NgSelect < Magicka::Select
-  with_attribute_locals :text_field, :reference_key
+module Magicka
+  class NgSelect < Magicka::Select
+    with_attribute_locals :text_field, :reference_key
+  end
 end
--- source/app/models/magicka/ng_select_text.rb
+++ source/app/models/magicka/ng_select_text.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-class Magicka::NgSelectText < Magicka::Text
-  with_attribute_locals :text_field, :options, :reference_key
+module Magicka
+  class NgSelectText < Magicka::Text
+    with_attribute_locals :text_field, :options, :reference_key
+  end
 end
