e1bba5595252005d94d9159bc5be8845d534e567
darthjee <darthjee@gmail.com>
2020-07-02 13:25:55 +0200

    Code style

--- source/app/assets/javascripts/filters/dig.js
+++ source/app/assets/javascripts/filters/dig.js
@@ -1,11 +1,24 @@
 (function(angular, _) {
+  class Digger {
+    constructor(input, keys) {
+      this.input = input;
+      this.keys = keys.split(".")
+    }
+
+    dig() {
+      return _.reduce(this.keys, this.reduce, this.input);
+    }
+
+    reduce(result, key) {
+      return result[key];
+    }
+  }
+
+  Digger.dig = function(input, keys) {
+    return new Digger(input, keys).dig();
+  }
+
   angular
     .module("plague_inc")
-    .filter("dig", function() {
-      return function(input, keys) {
-        return _.reduce(keys.split("."), function(result, key) {
-          return result[key];
-        }, input)
-      };
-    });
+    .filter("dig", function() { return Digger.dig });
 }(window.angular, window._));
