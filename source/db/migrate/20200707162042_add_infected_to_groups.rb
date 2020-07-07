49915a3e0003e88334df4ee106aba58bb20e4245
darthjee <darthjee@gmail.com>
2020-07-07 18:26:57 +0200

    Add Add infected to groups

---
+++ source/db/migrate/20200707162042_add_infected_to_groups.rb
@@ -0,0 +1,7 @@
+class AddInfectedToGroups < ActiveRecord::Migration[5.2]
+  def change
+    change_table :simulation_contagion_groups do |t|
+      t.integer :infected, null: false, default: 0
+    end
+  end
+end
