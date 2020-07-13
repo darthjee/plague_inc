13066c93fd7352ec918215ce348d8b063cf39f5f
darthjee <darthjee@gmail.com>
2020-07-13 12:15:09 +0200

    Rubocop fix

--- source/app/models/simulation/contagion/population.rb
+++ source/app/models/simulation/contagion/population.rb
@@ -3,7 +3,7 @@
 class Simulation < ApplicationRecord
   class Contagion < ApplicationRecord
     class Population < ApplicationRecord
-      STATES = %w[healthy infected immune]
+      STATES = %w[healthy infected immune].freeze
 
       scope :infected, -> { where(state: :infected) }
       scope :healthy, -> { where(state: :healthy) }
@@ -25,7 +25,7 @@ class Simulation < ApplicationRecord
                 presence: true,
                 numericality: {
                   greater_than_or_equal_to: 0,
-                  only_integer: true,
+                  only_integer: true
                 }
 
       validates :state,
--- source/db/migrate/20200713100107_remove_populations_infected_days.rb
+++ source/db/migrate/20200713100107_remove_populations_infected_days.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 class RemovePopulationsInfectedDays < ActiveRecord::Migration[5.2]
   def up
     change_table :simulation_contagion_populations do |t|
       t.remove :infected_days
-      t.index %i[instant_id group_id days], name: :simulation_contagion_unique_keys, unique: true
+      t.index %i[instant_id group_id days],
+              name: :simulation_contagion_unique_keys, unique: true
       t.remove_index name: :simulation_contagion_unique
     end
   end
@@ -10,7 +13,8 @@ class RemovePopulationsInfectedDays < ActiveRecord::Migration[5.2]
   def down
     change_table :simulation_contagion_populations do |t|
       t.integer :infected_days
-      t.index %i[instant_id group_id infected_days], name: :simulation_contagion_unique, unique: true
+      t.index %i[instant_id group_id infected_days],
+              name: :simulation_contagion_unique, unique: true
       t.remove_index name: :simulation_contagion_unique_keys
     end
   end
