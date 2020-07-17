b14a0e3d46512b632767659dbef5500b65d44774
darthjee <darthjee@gmail.com>
2020-07-17 17:41:07 +0200

    Add interactions to populations

---
+++ source/db/migrate/20200717145935_add_interactions_to.rb
@@ -0,0 +1,7 @@
+class AddInteractionsTo < ActiveRecord::Migration[5.2]
+  def change
+    change_table :simulation_contagion_populations do |t|
+      t.integer :interactions, null: false
+    end
+  end
+end
