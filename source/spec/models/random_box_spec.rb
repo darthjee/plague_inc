1800207553eb5ace9824689b1174ab0e59a15fbe
darthjee <darthjee@gmail.com>
2020-07-20 14:04:41 +0200

    Rubocop fix

--- source/spec/models/random_box_spec.rb
+++ source/spec/models/random_box_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe RandomBox do
@@ -133,23 +135,23 @@ describe RandomBox do
 
   describe 'method missing' do
     context 'when not sending arguments' do
-      it { expect(subject.rate).to be_in((0...1)) }
+      it { expect(random_box.rate).to be_in((0...1)) }
 
-      it { expect(subject.rate).to be_a(Float) }
+      it { expect(random_box.rate).to be_a(Float) }
     end
 
     context 'when sending integer argument' do
-      it { expect(subject.person(100)).to be_in((0...100)) }
+      it { expect(random_box.person(100)).to be_in((0...100)) }
 
-      it { expect(subject.id(100)).to be_a(Integer) }
+      it { expect(random_box.id(100)).to be_a(Integer) }
     end
 
     context 'when sending range argument' do
       let(:range) { (100..200) }
 
-      it { expect(subject.choice(range)).to be_in((range)) }
+      it { expect(random_box.choice(range)).to be_in(range) }
 
-      it { expect(subject.selected(range)).to be_a(Integer) }
+      it { expect(random_box.selected(range)).to be_a(Integer) }
     end
   end
 end
