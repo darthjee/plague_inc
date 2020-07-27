598f0a27e2ea7c010feb3f9cb07130faba9e0e4c
darthjee <darthjee@gmail.com>
2020-07-27 10:51:33 +0200

    Use processor in post creator

--- source/app/processors/simulation/contagion/post_creator.rb
+++ source/app/processors/simulation/contagion/post_creator.rb
@@ -5,14 +5,13 @@ class Simulation < ApplicationRecord
     # @author darthjee
     #
     # Processor to be run after creation of an instant
-    class PostCreator
-      # Kills and heals populations after creation of instant
-      #
+    class PostCreator < ::Processor
       # @param instant [Instant] instant being processed
-      def self.process(instant)
-        new(instant).process
+      def initialize(instant)
+        @instant = instant
       end
 
+      # Kills and heals populations after creation of instant
       def process
         Killer.process(instant)
         Healer.process(instant)
@@ -31,10 +30,6 @@ class Simulation < ApplicationRecord
       delegate :contagion, to: :instant
       delegate :days_till_recovery, to: :contagion
       delegate :populations, to: :instant
-
-      def initialize(instant)
-        @instant = instant
-      end
     end
   end
 end
