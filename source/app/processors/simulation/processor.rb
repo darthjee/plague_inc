49f4f294e53fecd958d28e5e26cc28e276a30462
darthjee <darthjee@gmail.com>
2020-08-22 18:54:52 +0200

    Add processor

---
+++ source/app/processors/simulation/processor.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Simulation < ApplicationRecord
+  class Processor
+    include ::Processor
+
+    def initialize(simulation, times: 1)
+      @simulation = simulation
+      @times      = times
+    end
+
+    def process
+      times.times do
+        settings.process
+      end
+    end
+
+    private
+
+    attr_reader :simulation, :times
+    delegate :settings, to: :simulation
+  end
+end
