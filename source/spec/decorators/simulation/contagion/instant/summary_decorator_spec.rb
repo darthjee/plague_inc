0b067c843795cd81a5dda336417502a74ae1e82c
darthjee <darthjee@gmail.com>
2020-09-02 16:50:11 +0200

    Extract shared example

--- source/spec/decorators/simulation/contagion/instant/summary_decorator_spec.rb
+++ source/spec/decorators/simulation/contagion/instant/summary_decorator_spec.rb
@@ -2,6 +2,82 @@
 
 require 'spec_helper'
 
+shared_examples 'a contagion instant summary count of population' do |state|
+  let(:result) { decorator.public_send(state) }
+
+  context 'when there is no population' do
+    it { expect(result).to be_zero }
+  end
+
+  context "when there are only non-#{state} populations" do
+    let(:populations) { Random.rand(2..10) }
+    let(:states) do
+      Simulation::Contagion::Population::STATES -
+        [state.to_s]
+    end
+    let(:sizes) do
+      populations.times.map { Random.rand(100..500) }
+    end
+
+    before do
+      sizes.each.with_index do |size, days|
+        create(
+          :contagion_population,
+          state: states.sample,
+          instant: instant,
+          group: group,
+          behavior: behavior,
+          days: days,
+          size: size
+        )
+      end
+    end
+
+    it { expect(result).to be_zero }
+  end
+
+  context "when there are #{state} and non-#{state} populations" do
+    let(:other_populations) { Random.rand(2..10) }
+    let(:populations)       { Random.rand(2..10) }
+    let(:sizes) do
+      populations.times.map { Random.rand(100..500) }
+    end
+    let(:alive_states) do
+      Simulation::Contagion::Population::STATES -
+        [state.to_s]
+    end
+
+    before do
+      other_populations.times.each do |days|
+        create(
+          :contagion_population,
+          state: alive_states.sample,
+          instant: instant,
+          group: group,
+          behavior: behavior,
+          days: days
+        )
+      end
+
+      sizes.each.with_index do |size, days|
+        create(
+          :contagion_population, state,
+          instant: instant,
+          group: group,
+          behavior: behavior,
+          days: days,
+          size: size
+        )
+      end
+    end
+
+
+    it 'returns the sum of dead sizes' do
+      expect(result).to eq(sizes.sum)
+    end
+  end
+end
+
 describe Simulation::Contagion::Instant::SummaryDecorator do
   subject(:decorator) { described_class.new(instant) }
 
@@ -49,76 +125,6 @@ describe Simulation::Contagion::Instant::SummaryDecorator do
   end
 
   describe '#dead' do
-    context 'when there is no population' do
-      it { expect(decorator.dead).to be_zero }
-    end
-
-    context 'when there are only alive populations' do
-      let(:populations) { Random.rand(2..10) }
-      let(:states) do
-        Simulation::Contagion::Population::STATES -
-          [Simulation::Contagion::Population::DEAD]
-      end
-      let(:sizes) do
-        populations.times.map { Random.rand(100..500) }
-      end
-
-      before do
-        sizes.each.with_index do |size, days|
-          create(
-            :contagion_population,
-            state: states.sample,
-            instant: instant,
-            group: group,
-            behavior: behavior,
-            days: days,
-            size: size
-          )
-        end
-      end
-
-      it { expect(decorator.dead).to be_zero }
-    end
-
-    context 'when there are dead and non-dead populations' do
-      let(:other_populations) { Random.rand(2..10) }
-      let(:populations) { Random.rand(2..10) }
-      let(:sizes) do
-        populations.times.map { Random.rand(100..500) }
-      end
-      let(:alive_states) do
-        Simulation::Contagion::Population::STATES -
-          [Simulation::Contagion::Population::DEAD]
-      end
-
-      before do
-        other_populations.times.each do |days|
-          create(
-            :contagion_population,
-            state: alive_states.sample,
-            instant: instant,
-            group: group,
-            behavior: behavior,
-            days: days
-          )
-        end
-
-        sizes.each.with_index do |size, days|
-          create(
-            :contagion_population, :dead,
-            instant: instant,
-            group: group,
-            behavior: behavior,
-            days: days,
-            size: size
-          )
-        end
-      end
-
-
-      it 'returns the sum of dead sizes' do
-        expect(decorator.dead).to eq(sizes.sum)
-      end
-    end
+    it_behaves_like 'a contagion instant summary count of population', :dead
   end
 end
