610783ed454d85816156cb22a7525cb352497bf9
darthjee <darthjee@gmail.com>
2020-09-17 23:25:29 +0200

    Load data in partials

--- source/app/assets/javascripts/controllers/simulation/graph_controller.js
+++ source/app/assets/javascripts/controllers/simulation/graph_controller.js
@@ -8,6 +8,7 @@
     this.id       = $routeParams.id;
     this.location = $location;
 
+    _.bindAll(this, '_summaryPath', '_summaryUrl', '_loadData', '_setSimulation');
     this._loadData();
   }
 
@@ -15,19 +16,38 @@
 
   fn._setSimulation = function(data) {
     if (this.simulation) {
-    } else {
-      console.info(data)
+      data.instants = this.simulation.instants
+        .concat(data.instants);
+
       this.simulation = data;
+    } else {
+      this.simulation = data;
+    }
+
+    if (data.instants.length < data.instants_total) {
+      this._loadData();
     }
   };
 
   fn._loadData = function() {
-    var promisse = this.http.get(this.sumaryPath());
+    var promisse = this.http.get(this._summaryUrl());
 
     promisse.success(this._setSimulation);
   };
 
-  fn.sumaryPath = function() {
+  fn._summaryUrl = function() {
+    var path = this._summaryPath();
+
+    if (this.simulation && this.simulation.instants.length > 0) {
+      var lastInstanId = this.simulation.instants.slice(-1).pop().id
+
+      return path + "?pagination[last_instant_id]="+lastInstanId;
+    } else {
+      return path;
+    }
+  };
+
+  fn._summaryPath = function() {
     return this.location.$$path + "/contagion/summary"
   };
 
