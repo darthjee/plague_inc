51e02df22459e9ebd8fa85d6177c5d6170c10c03
darthjee <darthjee@gmail.com>
2020-10-11 13:42:18 +0200

    Show more information

--- source/config/initializers/magicka.rb
+++ source/config/initializers/magicka.rb
@@ -3,3 +3,4 @@
 Magicka::Form.with_element(Magicka::NgSelect)
 Magicka::Display.with_element(Magicka::NgSelectText, :ng_select)
 Magicka::Form.with_element(Magicka::NgButton)
+Magicka::Display.with_element(Magicka::NgButton)
