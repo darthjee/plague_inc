aac1e2bcf4b9fb30601e50ce72ecc7b316aa3fac
darthjee <darthjee@gmail.com>
2020-11-15 00:09:18 +0100

    Add expectation

--- source/spec/processors/simulation/contagion/initializer_spec.rb
+++ source/spec/processors/simulation/contagion/initializer_spec.rb
@@ -9,9 +9,12 @@ describe Simulation::Contagion::Initializer do
     create(:contagion_instant, contagion: contagion, day: 9)
   end
 
-  let(:group)    { contagion.groups.last }
-  let(:behavior) { group.behavior }
-  let(:days)     { Random.rand(1..10) }
+  let(:group)     { contagion.groups.last }
+  let(:behavior)  { group.behavior }
+  let(:days)      { Random.rand(1..10) }
+  let(:dead_size) { Random.rand(1..100) }
+  
+  let(:new_instant) { described_class.process(instant) }
 
   let(:interesting_populations) do
     [dead_population, immune_population, infected_population]
@@ -49,7 +52,7 @@ describe Simulation::Contagion::Initializer do
       state: :dead,
       group: group,
       behavior: behavior,
-      size: Random.rand(100),
+      size: dead_size,
       instant: instant,
       interactions: 10,
       days: days
@@ -71,64 +74,92 @@ describe Simulation::Contagion::Initializer do
 
   describe '.process' do
     it 'updates simulation' do
-      expect { described_class.process(instant) }
+      expect { new_instant }
         .to(change { simulation.reload.updated_at })
     end
 
     it 'does not update simulation status' do
-      expect { described_class.process(instant) }
+      expect { new_instant }
         .not_to(change { simulation.reload.status })
     end
 
     it do
-      expect { described_class.process(instant) }
+      expect { new_instant }
         .to change { instant.reload.status }
         .to(Simulation::Contagion::Instant::PROCESSING)
     end
 
     it do
-      expect(described_class.process(instant))
+      expect(new_instant)
         .to be_a(Simulation::Contagion::Instant)
     end
 
     it do
-      expect(described_class.process(instant))
+      expect(new_instant)
         .to be_persisted
     end
 
     it 'creates new instant' do
-      expect(described_class.process(instant))
+      expect(new_instant)
         .not_to eq(instant)
     end
 
     it 'creates new instant for a new day' do
-      expect(described_class.process(instant).day)
+      expect(new_instant.day)
         .to eq(10)
     end
 
     it 'creates populations for all infected populations' do
-      expect(described_class.process(instant).populations)
+      expect(new_instant.populations)
         .not_to be_empty
     end
 
     it 'persists all populations' do
-      expect(described_class.process(instant).populations)
+      expect(new_instant.populations)
         .to all(be_persisted)
     end
 
     it 'makes population for the important populations' do
-      expect(described_class.process(instant).populations.sort.pluck(:state))
+      expect(new_instant.populations.sort.pluck(:state))
         .to eq(%w[dead immune infected])
     end
 
     it 'increments days counters' do
-      expect(described_class.process(instant).populations.pluck(:days))
+      expect(new_instant.populations.pluck(:days))
         .to all(eq(days + 1))
     end
 
     it 'makes population with correct size' do
-      expect(described_class.process(instant).populations.sort.pluck(:size))
+      expect(new_instant.populations.sort.pluck(:size))
         .to eq(interesting_populations.map(&:size))
+    end
+
+    context 'when there are more than one dead population' do
+      let(:days)          { 0 }
+      let(:old_size)      { Random.rand(1..100) }
+      let(:expected_size) { dead_size + old_size }
+
+      let(:state) do
+        Simulation::Contagion::Population::DEAD
+      end
+
+      let!(:old_populations) do
+        create(
+          :contagion_population,
+          interactions: 0,
+          instant: instant,
+          group: group,
+          behavior: behavior,
+          size: old_size,
+          state: state,
+          days: 1
+        )
+      end
+
+      it do
+        expect(new_instant.populations.dead.size)
+          .to eq(1)
+      end
     end
   end
 end
