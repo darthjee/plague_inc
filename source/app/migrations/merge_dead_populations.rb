2fe4486c5fc205f828d74785e778a558ff5e6e3f
darthjee <darthjee@gmail.com>
2020-11-15 23:51:33 +0100

    Rubycritic fix

--- source/app/migrations/merge_dead_populations.rb
+++ source/app/migrations/merge_dead_populations.rb
@@ -5,25 +5,13 @@ class MergeDeadPopulations
 
   def process
     instants.find_each do |instant|
-      process_populations(instant.populations.dead)
+      InstantProcessor.process(instant: instant)
     end
   end
+
+  private
 
   def instants
     @instants ||= Simulation::Contagion::Instant
-  end
-
-  def process_populations(populations)
-    populations.group(:group_id).sum(:size).each do |group_id, size|
-      process_group(populations, group_id, size)
-    end
-  end
-
-  def process_group(populations, group_id, size)
-    MergeGroup.process(
-      populations: populations,
-      group_id: group_id,
-      size: size
-    )
   end
 end
