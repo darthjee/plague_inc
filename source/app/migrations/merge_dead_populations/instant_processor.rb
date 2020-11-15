81c0121ae4674eac318819c6c03a1fba47dfce0a
darthjee <darthjee@gmail.com>
2020-11-16 00:03:06 +0100

    Add missing class

---
+++ source/app/migrations/merge_dead_populations/instant_processor.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class MergeDeadPopulations
+  class InstantProcessor < Sinclair::Options
+    include Processor
+
+    with_options :instant
+
+    def process
+      grouped_populations.sum(:size).each do |group_id, size|
+        MergeGroup.process(
+          populations: populations,
+          group_id: group_id,
+          size: size
+        )
+      end
+    end
+
+    private
+
+    def grouped_populations
+      populations.group(:group_id)
+    end
+
+    def populations
+      @populations ||= instant.populations.dead
+    end
+  end
+end
