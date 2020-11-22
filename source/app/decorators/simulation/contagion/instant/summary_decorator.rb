58219d24843b75b7e4a5be105f4e82fe1f9c67ce
darthjee <darthjee@gmail.com>
2020-11-22 19:03:54 +0100

    Add percentages

--- source/app/decorators/simulation/contagion/instant/summary_decorator.rb
+++ source/app/decorators/simulation/contagion/instant/summary_decorator.rb
@@ -10,30 +10,57 @@ class Simulation < ApplicationRecord
         expose :id
         expose :status
         expose :day
+
         expose :total
+
         expose :dead
         expose :infected
         expose :immune
         expose :healthy
 
+        expose :dead_percentage
+        expose :infected_percentage
+        expose :immune_percentage
+        expose :healthy_percentage
+
         def total
-          scoped_size(:all)
+          @total ||= scoped_size(:all)
         end
 
         def dead
-          scoped_size(:dead)
+          @dead ||= scoped_size(:dead)
         end
 
         def infected
-          scoped_size(:infected)
+          @infected ||= scoped_size(:infected)
         end
 
         def immune
-          scoped_size(:immune)
+          @immune ||= scoped_size(:immune)
         end
 
         def healthy
-          scoped_size(:healthy)
+          @healthy ||= scoped_size(:healthy)
+        end
+
+        def dead_percentage
+          return 0 unless total.positive?
+          dead.to_f / total
+        end
+
+        def infected_percentage
+          return 0 unless total.positive?
+          infected.to_f / total
+        end
+
+        def immune_percentage
+          return 0 unless total.positive?
+          immune.to_f / total
+        end
+
+        def healthy_percentage
+          return 0 unless total.positive?
+          healthy.to_f / total
         end
 
         private
