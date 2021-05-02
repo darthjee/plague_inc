b7c52ed9484ddee2262692559cb93c6f4d3bedd7
darthjee <darthjee@gmail.com>
2021-05-02 18:43:29 +0200

    Add plot class

---
+++ source/app/models/simulation/graph/plot.rb
@@ -0,0 +1,7 @@
+class Simulation < ApplicationRecord
+  class Graph < ApplicationRecord
+    class Plot < ApplicationRecord
+      validates_presence_of :label, :attribute, :metric
+    end
+  end
+end
