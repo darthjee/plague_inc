445e364d3076780ff335d8991b2b5cbd34598e75
darthjee <darthjee@gmail.com>
2021-05-03 17:21:18 +0200

    Correct variable name

--- source/spec/models/simulation/graph/plot_spec.rb
+++ source/spec/models/simulation/graph/plot_spec.rb
@@ -3,37 +3,37 @@
 require 'spec_helper'
 
 describe Simulation::Graph::Plot, type: :model do
-  subject(:simulation) { build(:simulation_graph_plot) }
+  subject(:plot) { build(:simulation_graph_plot) }
 
   describe 'validations' do
     it do
-      expect(simulation).to validate_presence_of(:graph)
+      expect(plot).to validate_presence_of(:graph)
     end
 
     it do
-      expect(simulation).to validate_presence_of(:label)
+      expect(plot).to validate_presence_of(:label)
     end
 
     it do
-      expect(simulation).to validate_length_of(:label)
+      expect(plot).to validate_length_of(:label)
         .is_at_most(255)
     end
 
     it do
-      expect(simulation).to validate_presence_of(:field)
+      expect(plot).to validate_presence_of(:field)
     end
 
     it do
-      expect(simulation).to validate_inclusion_of(:field)
+      expect(plot).to validate_inclusion_of(:field)
         .in_array(described_class::FIELDS)
     end
 
     it do
-      expect(simulation).to validate_presence_of(:metric)
+      expect(plot).to validate_presence_of(:metric)
     end
 
     it do
-      expect(simulation).to validate_inclusion_of(:metric)
+      expect(plot).to validate_inclusion_of(:metric)
         .in_array(described_class::METRICS)
     end
   end
--- source/spec/models/simulation/graph_spec.rb
+++ source/spec/models/simulation/graph_spec.rb
@@ -3,15 +3,15 @@
 require 'spec_helper'
 
 describe Simulation::Graph, type: :model do
-  subject(:simulation) { build(:simulation_graph) }
+  subject(:graph) { build(:simulation_graph) }
 
   describe 'validations' do
     it do
-      expect(simulation).to validate_presence_of(:name)
+      expect(graph).to validate_presence_of(:name)
     end
 
     it do
-      expect(simulation).to validate_length_of(:name)
+      expect(graph).to validate_length_of(:name)
         .is_at_most(255)
     end
   end
