af4e6f27a3a07a333f25b6f107f048aba427b523
darthjee <darthjee@gmail.com>
2021-05-03 16:16:34 +0200

    Add limit to migration

--- source/db/migrate/20210502133349_create_simulation_graph_plots.rb
+++ source/db/migrate/20210502133349_create_simulation_graph_plots.rb
@@ -4,7 +4,7 @@ class CreateSimulationGraphPlots < ActiveRecord::Migration[5.2]
   def change
     create_table :simulation_graph_plots do |t|
       t.string :label, null: false
-      t.string :field, null: false
+      t.string :field, null: false, limit: 19
       t.string :metric, null: false
     end
   end
