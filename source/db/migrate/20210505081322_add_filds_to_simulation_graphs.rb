e113464b07ac66e17fff2da66bd5f4a6dc14085e
darthjee <darthjee@gmail.com>
2021-05-12 10:38:43 +0200

    Add migrations for graphs

---
+++ source/db/migrate/20210505081322_add_filds_to_simulation_graphs.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddFildsToSimulationGraphs < ActiveRecord::Migration[5.2]
+  def change
+    change_table :simulation_graphs do |t|
+      t.string :title
+    end
+  end
+end
