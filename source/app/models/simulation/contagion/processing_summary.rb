af3ffa5868702f260b0c7eb34ce8d55a2b2e5ffc
darthjee <darthjee@gmail.com>
2021-05-16 23:32:59 +0200

    Rubocop fix

--- source/app/models/simulation/contagion/processing_summary.rb
+++ source/app/models/simulation/contagion/processing_summary.rb
@@ -4,16 +4,18 @@ class Simulation < ApplicationRecord
   class Contagion < ApplicationRecord
     class ProcessingSummary
       attr_reader :instant, :interactions
-      
+
       delegate :id, :status, :day,
-        :populations, to: :instant
+               :populations, to: :instant
 
       def initialize(instant, interactions: 0)
         @instant = instant
+        @interactions = interactions
       end
 
       def ==(other)
         return false unless other.is_a?(self.class)
+
         other.interactions == interactions &&
           other.instant == instant
       end
