1202a42555336c9c00a193607489a9cbbe0079f8
darthjee <darthjee@gmail.com>
2021-07-09 23:24:20 +0200

    Mock worker

--- source/spec/spec_helper.rb
+++ source/spec/spec_helper.rb
@@ -118,6 +118,7 @@ RSpec.configure do |config|
 
     ActiveRecord::Migration.check_pending!
     ActiveRecord::Migration.maintain_test_schema!
+    allow(Simulation::ProcessorWorker).to receive(:perform_async)
   end
 
   config.around do |example|
