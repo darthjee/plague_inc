8f66eeac1c27aba338b117de729230f9f3f51f54
darthjee <darthjee@gmail.com>
2021-07-12 00:21:08 +0200

    Add test for day 1

--- source/spec/processors/simulation/contagion/reparator_spec.rb
+++ source/spec/processors/simulation/contagion/reparator_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 fdescribe Simulation::Contagion::Reparator do
-  subject(:process) { described_class.process(simulation_id, 0) }
+  subject(:process) { described_class.process(simulation_id, day) }
 
   let(:simulation_id) { simulation.id }
   let(:simulation)   { contagion.simulation }
@@ -12,6 +12,7 @@ fdescribe Simulation::Contagion::Reparator do
   let(:group)        { contagion.groups.first }
 
   describe '.process' do
+    let(:day) { 0 }
     let!(:day_0) do
       create(:contagion_instant, day: 0, contagion: contagion)
     end
@@ -19,7 +20,7 @@ fdescribe Simulation::Contagion::Reparator do
     before do
       create(
         :contagion_population, :infected,
-        size: size / 2,
+        size: size / 4,
         instant: day_0,
         group: group
       )
@@ -41,6 +42,39 @@ fdescribe Simulation::Contagion::Reparator do
         expect { process }
           .to change(Simulation::Contagion::Population, :count)
           .by(-1)
+      end
+    end
+
+    context "when there are 2 instants one instant" do
+      let(:day) { 1 }
+      let!(:day_1) do
+        create(:contagion_instant, day: 1, contagion: contagion)
+      end
+
+      before do
+        create(
+          :contagion_population, :infected,
+          size: size / 2,
+          instant: day_1,
+          group: group
+        )
+      end
+
+      it do
+        expect { process }.to change { simulation.reload.status }
+          .to(Simulation::CREATED)
+      end
+
+      it "removes all instants" do
+        expect { process }
+          .to change { simulation.reload.contagion.instants.size }
+          .to(0)
+      end
+
+      it "removes populations" do
+        expect { process }
+          .to change(Simulation::Contagion::Population, :count)
+          .by(-2)
       end
     end
   end
