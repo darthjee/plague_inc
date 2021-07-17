a6f227ed0e0063a7954c951b10f743ba6e39dcc1
darthjee <darthjee@gmail.com>
2021-07-17 13:07:27 -0300

    Eager load in controller

--- source/app/workers/simulation/processor_worker.rb
+++ source/app/workers/simulation/processor_worker.rb
@@ -4,7 +4,12 @@ class Simulation < ApplicationRecord
   class ProcessorWorker
     include Sidekiq::Worker
     def perform(id)
-      simulation = Simulation.find(id)
+      simulation = Simulation
+        .eager_load(:contagion)
+        .eager_load(contagion: :groups)
+        .eager_load(contagion: :behaviors)
+        .find(id)
+
       Processor.process(simulation, times: 1)
 
       return if simulation.finished?
