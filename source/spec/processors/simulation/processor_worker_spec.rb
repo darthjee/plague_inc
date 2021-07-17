2b85f0cad38a6c2dd948ae3f963ee1ff957a2232
darthjee <darthjee@gmail.com>
2021-07-17 13:42:10 -0300

    Remove focus

--- source/spec/processors/simulation/processor_worker_spec.rb
+++ source/spec/processors/simulation/processor_worker_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-fdescribe Simulation::ProcessorWorker, :contagion_cache do
+describe Simulation::ProcessorWorker, :contagion_cache do
   subject(:worker) { described_class.new }
 
   let(:simulation_id) { simulation.id }
