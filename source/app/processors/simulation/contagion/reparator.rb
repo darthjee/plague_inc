fb27aa393249c6f67041bca83a85c9f397f449df
darthjee <darthjee@gmail.com>
2021-08-02 20:31:48 -0300

    Refactor

--- source/app/processors/simulation/contagion/reparator.rb
+++ source/app/processors/simulation/contagion/reparator.rb
@@ -8,33 +8,67 @@ class Simulation < ApplicationRecord
       include ::Processor
       include Contagion::Cacheable
 
-      def self.check_and_fix_all
-        check_all
-        repair_all
-      end
+      class << self
+        def check_and_fix_all
+          check_all
+          repair_all
+        end
 
-      def self.check_all
-        Simulation::Contagion
-          .eager_load(:groups)
-          .eager_load(:simulation)
-          .eager_load(:current_instant)
-          .eager_load(current_instant: :populations)
-          .where(simulations: { status: :finished, checked: false })
-          .each do |c|
-            current_size = c.current_instant.populations.map(&:size).sum
-            size = c.groups.map(&:size).sum
-            c.simulation.update(checked: current_size == size)
+        def check_all
+          simulations_to_check.each { |simulation| check(simulation) }
+        end
+
+        def repair_all
+          broken_simulations_information.each do |id, day|
+            Simulation::Contagion::Reparator.process(id, day)
+            Simulation::ProcessorWorker.perform_async(id)
           end
-      end
+        end
 
-      def self.repair_all
-        Simulation.eager_load(:contagion).eager_load(contagion: :groups).where(status: :finished, checked: false).map do |simulation|
+        private
+
+        def check(simulation)
+          contagion = simulation.contagion
+          current_size = contagion.current_instant.populations.map(&:size).sum
+          size = contagion.groups.map(&:size).sum
+          contagion.simulation.update(checked: current_size == size)
+        end
+
+        def simulations_to_check
+          scoped_simulations
+            .eager_load(contagion: :current_instant)
+            .eager_load(contagion: { current_instant: :populations })
+        end
+
+        def broken_simulations_information
+          scoped_simulations.map do |simulation|
+            broken_simulation_information(simulation)
+          end.compact
+        end
+
+        def broken_simulation_information(simulation)
           size = simulation.contagion.groups.map(&:size).sum
-          day = simulation.contagion.instants.joins(:populations).group(:instant_id).order(:day).having('sum(size) <> ?', size).pluck(:day).first
-          [simulation.id, day]
-        end.select { |_id, day| day }.each do |id, day|
-          Simulation::Contagion::Reparator.process(id, day - 1)
-          Simulation::ProcessorWorker.perform_async(id)
+          day = broken_day(simulation.contagion.instants, size)
+          return unless day
+
+          [simulation.id, day - 1]
+        end
+
+        def broken_day(instants, size)
+          instants
+            .joins(:populations)
+            .group(:instant_id)
+            .order(:day)
+            .having('sum(size) <> ?', size)
+            .pluck(:day)
+            .first
+        end
+
+        def scoped_simulations
+          Simulation
+            .eager_load(:contagion)
+            .eager_load(contagion: :groups)
+            .where(status: :finished, checked: false)
         end
       end
 
