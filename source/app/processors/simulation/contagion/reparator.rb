ba934f53f5c095a5d0a0461677667bf9a5897c09
darthjee <darthjee@gmail.com>
2021-08-02 21:11:35 -0300

    Extract cleass methods

--- source/app/processors/simulation/contagion/reparator.rb
+++ source/app/processors/simulation/contagion/reparator.rb
@@ -1,76 +1,17 @@
 # frozen_string_literal: true
 
 require './app/processors/simulation/contagion/population/builder'
+require './app/processors/simulation/contagion/reparator/class_methods'
 
 class Simulation < ApplicationRecord
   class Contagion < ApplicationRecord
     class Reparator
+      class << self
+        include Reparator::ClassMethods
+      end
+
       include ::Processor
       include Contagion::Cacheable
-
-      class << self
-        def check_and_fix_all
-          check_all
-          repair_all
-        end
-
-        def check_all
-          simulations_to_check.each { |simulation| check(simulation) }
-        end
-
-        def repair_all
-          broken_simulations_information.each do |id, day|
-            Simulation::Contagion::Reparator.process(id, day)
-            Simulation::ProcessorWorker.perform_async(id)
-          end
-        end
-
-        private
-
-        def check(simulation)
-          contagion = simulation.contagion
-          current_size = contagion.current_instant.populations.map(&:size).sum
-          size = contagion.groups.map(&:size).sum
-          contagion.simulation.update(checked: current_size == size)
-        end
-
-        def simulations_to_check
-          scoped_simulations
-            .eager_load(contagion: :current_instant)
-            .eager_load(contagion: { current_instant: :populations })
-        end
-
-        def broken_simulations_information
-          scoped_simulations.map do |simulation|
-            broken_simulation_information(simulation)
-          end.compact
-        end
-
-        def broken_simulation_information(simulation)
-          size = simulation.contagion.groups.map(&:size).sum
-          day = broken_day(simulation.contagion.instants, size)
-          return unless day
-
-          [simulation.id, day - 1]
-        end
-
-        def broken_day(instants, size)
-          instants
-            .joins(:populations)
-            .group(:instant_id)
-            .order(:day)
-            .having('sum(size) <> ?', size)
-            .pluck(:day)
-            .first
-        end
-
-        def scoped_simulations
-          Simulation
-            .eager_load(:contagion)
-            .eager_load(contagion: :groups)
-            .where(status: :finished, checked: false)
-        end
-      end
 
       def initialize(simulation_id, day)
         @simulation_id = simulation_id
