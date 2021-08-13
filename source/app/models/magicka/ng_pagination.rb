9887bbb40519b91fb0c0c9570a6a31a763c9d442
darthjee <darthjee@gmail.com>
2021-08-13 18:35:27 -0300

    Extract next and previous pages

--- source/app/models/magicka/ng_pagination.rb
+++ source/app/models/magicka/ng_pagination.rb
@@ -8,6 +8,7 @@ module Magicka
     with_attribute_locals :path_method, :options
     with_attributes :model, :field
     with_locals :pagination, :current_page, :total_pages
+    with_locals :previous_page, :next_page
 
     def pagination
       [model, field].join('.')
@@ -23,6 +24,14 @@ module Magicka
 
     def options
       @options ||= {}
+    end
+
+    def previous_page
+      "{{ #{pagination}.page - 1 }}"
+    end
+
+    def next_page
+      "{{ #{pagination}.page + 1 }}"
     end
   end
 end
