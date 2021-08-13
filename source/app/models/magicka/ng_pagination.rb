2626dcec1cff1f702e13279c1d6b7f6b3d6fd772
darthjee <darthjee@gmail.com>
2021-08-13 17:48:32 -0300

    Extract current page

--- source/app/models/magicka/ng_pagination.rb
+++ source/app/models/magicka/ng_pagination.rb
@@ -7,10 +7,14 @@ module Magicka
 
     with_attribute_locals :path_method, :options
     with_attributes :model, :field
-    with_locals :pagination
+    with_locals :pagination, :current_page
 
     def pagination
       [model, field].join('.')
+    end
+
+    def current_page
+      [pagination, 'page'].join('.')
     end
   end
 end
