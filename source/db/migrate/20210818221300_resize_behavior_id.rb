d66818a60a4178f4157677c246260eb33a21a58e
darthjee <darthjee@gmail.com>
2021-08-18 19:41:40 -0300

    Finish migration

--- source/db/migrate/20210818221300_resize_behavior_id.rb
+++ source/db/migrate/20210818221300_resize_behavior_id.rb
@@ -1,11 +1,17 @@
 class ResizeBehaviorId < ActiveRecord::Migration[5.2]
   def up
     with_fk do
+      change_column :simulation_contagion_behaviors, :id, :integer, null: false, unique: true, auto_increment: true
+      change_column :simulation_contagion_groups, :behavior_id, :integer
+      change_column :simulation_contagion_populations, :behavior_id, :integer, null: false
     end
   end
 
   def down
     with_fk do
+      change_column :simulation_contagion_behaviors, :id, :bigint, null: false, unique: true, auto_increment: true
+      change_column :simulation_contagion_groups, :behavior_id, :bigint
+      change_column :simulation_contagion_populations, :behavior_id, :bigint, null: false
     end
   end
 
@@ -16,7 +22,6 @@ class ResizeBehaviorId < ActiveRecord::Migration[5.2]
     remove_foreign_key :simulation_contagion_populations, :simulation_contagion_behaviors
 
     yield
-  ensure
 
     add_foreign_key :simulation_contagion_groups, :simulation_contagion_behaviors, column: :behavior_id
     add_foreign_key :simulation_contagion_populations, :simulation_contagion_behaviors, column: :behavior_id
