5371bf2b1d4d646f2e372ecf0ad4df665517748d
darthjee <darthjee@gmail.com>
2021-08-18 14:15:10 -0300

    Rubocop fix

--- source/db/migrate/20210818170149_remove_timestamps.rb
+++ source/db/migrate/20210818170149_remove_timestamps.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class RemoveTimestamps < ActiveRecord::Migration[5.2]
-  TABLES=%i[
+  TABLES = %i[
     simulation_contagions
     simulation_contagion_groups
     simulation_contagion_behaviors
     simulation_contagion_instants
     simulation_contagion_populations
-  ]
+  ].freeze
 
   def change
     TABLES.each do |table|
