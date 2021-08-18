fb0f1d44e9d0c85e1e1f898f5ffeae89c735ec4f
darthjee <darthjee@gmail.com>
2021-08-18 15:05:31 -0300

    Fix migration

--- source/db/migrate/20210818171531_change_integer_size.rb
+++ source/db/migrate/20210818171531_change_integer_size.rb
@@ -2,25 +2,16 @@
 
 class ChangeIntegerSize < ActiveRecord::Migration[5.2]
   TABLE = :simulation_contagion_populations
-  SIZES = {
-    days: 2,
-    size: 4,
-    new_infections: 4
-  }.freeze
   DEFAULTS = {
     default: 0,
     null: false
   }.freeze
 
   def up
-    SIZES.each do |column, size|
-      change_column TABLE, column, :integer, limit: size, **DEFAULTS
-    end
+    change_column TABLE, :days, :integer, limit: 2, **DEFAULTS
   end
 
   def down
-    SIZES.each_key do |column|
-      change_column TABLE, column, :integer, **DEFAULTS
-    end
+    change_column TABLE, :days, :integer, **DEFAULTS
   end
 end
