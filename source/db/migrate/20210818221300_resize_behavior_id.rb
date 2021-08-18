ac797e20917de48b13a6d50934816db2b5c019fb
darthjee <darthjee@gmail.com>
2021-08-18 19:17:54 -0300

    Add empty migration

---
+++ source/db/migrate/20210818221300_resize_behavior_id.rb
@@ -0,0 +1,23 @@
+class ResizeBehaviorId < ActiveRecord::Migration[5.2]
+  def up
+    with_fk do
+    end
+  end
+
+  def down
+    with_fk do
+    end
+  end
+
+  private
+
+  def with_fk
+    remove_foreign_key :simulation_contagion_groups, :simulation_contagion_behaviors
+    remove_foreign_key :simulation_contagion_populations, :simulation_contagion_behaviors
+
+    yield
+
+    add_foreign_key :simulation_contagion_groups, :simulation_contagion_behaviors, column: :behavior_id
+    add_foreign_key :simulation_contagion_populations, :simulation_contagion_behaviors, column: :behavior_id
+  end
+end
