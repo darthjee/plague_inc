8f7f86d9f7fb68ff2fad7f0fcf6fe8209c75c1ef
darthjee <darthjee@gmail.com>
2021-08-24 06:54:53 -0300

    Add variables

--- source/app/models/settings.rb
+++ source/app/models/settings.rb
@@ -7,6 +7,11 @@ class Settings
   settings_prefix 'PLAGUE_INC'
 
   with_settings(
+    :graph_host,
+    :graph_username,
+    :graph_key,
+    :graph_folder,
+
     title: 'Plague Simulations',
     favicon: 'http://images.coronasim.xyz/favicon.ico',
     cache_age: 10.seconds,
