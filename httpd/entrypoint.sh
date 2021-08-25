d0f4466f46ab30c8253408e9a20caf8b1a64f3aa
darthjee <darthjee@gmail.com>
2021-08-25 12:22:06 -0300

    Create functions

--- httpd/entrypoint.sh
+++ httpd/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "----------------------------------"
-echo "Initing SSHD"
-exec /usr/sbin/sshd -D -e &
-echo "----------------------------------"
-echo "Initing HTTPD"
-httpd-foreground
+function init_sshd() {
+  echo "----------------------------------"
+  echo "Initing SSHD"
+  exec /usr/sbin/sshd -D -e
+}
+
+function init_httpd() {
+  echo "----------------------------------"
+  echo "Initing HTTPD"
+  httpd-foreground
+}
+
+init_sshd & init_httpd
