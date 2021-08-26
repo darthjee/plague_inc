2135569f267153c2735f77361958022b1484ab9f
darthjee <darthjee@gmail.com>
2021-08-26 14:52:49 -0300

    Reschedule

--- source/app/workers/simulation/contagion/population_new_ids_worker.rb
+++ source/app/workers/simulation/contagion/population_new_ids_worker.rb
@@ -8,11 +8,14 @@ class Simulation < ApplicationRecord
       sidekiq_options queue: :super_critical
 
       def perform
-        return unless Simulation::Contagion::Population.where(new_id: nil).any?
-        new_id = Simulation::Contagion::Population.maximum(:new_id) + 1
-        Simulation::Contagion::Population.where(new_id: nil).limit(1).update_all(new_id: new_id)
+        if Simulation::Contagion::Population.where(new_id: nil).any?
+          new_id = Simulation::Contagion::Population.maximum(:new_id) + 1
+          Simulation::Contagion::Population.where(new_id: nil).limit(1).update_all(new_id: new_id)
 
-        self.class.perform_async
+          self.class.perform_async
+        else
+          self.class.perform_in(60.seconds)
+        end
       end
     end
   end
