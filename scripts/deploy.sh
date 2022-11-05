9d0a66f65e9779fc9a4757b419167adb683de1c9
darthjee <darthjee@gmail.com>
2022-11-05 20:58:46 -0300

    Fetch deploys

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,8 +9,18 @@ function services() {
     --header "Authorization: Bearer $RENDER_API_KEY"
 }
 
-function service_id() {
-  services | jq ".[] | select(.service.name == \"$RENDER_SERVICE_NAME\") | .service.id"
+function deploy() {
+  SERVICE_ID=$(service_id)
+  curl --request GET \
+    --url "https://api.render.com/v1/services/$SERVICE_ID/deploys" \
+    --header 'Accept: application/json' \
+    --header "Authorization: Bearer $RENDER_API_KEY"
 }
 
-service_id
+function service_id() {
+  services | jq \
+    ".[] | select(.service.name == \"$RENDER_SERVICE_NAME\") | .service.id" \
+    | sed -e 's/"//g' 
+}
+
+deploy
