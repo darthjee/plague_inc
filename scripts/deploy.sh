fcee48f0133d50083b6a08d68b7b86108ed2c549
darthjee <darthjee@gmail.com>
2022-11-05 21:09:08 -0300

    Check if it is the final commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+function isLatestCommit() {
+  VERSION=$(git tag | grep $(git describe  --tags))
+
+  if [[ $VERSION ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+if $(isLatestCommit); then
+  echo "latest commit";
+else
+  echo "Not last commit"
+  exit 0
+fi
+
 RENDER_SERVICE_NAME="plague-inc"
 
 function request() {
