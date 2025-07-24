9e62870d1bc20e78ffb3797471fa8902b0195a0e
darthjee <darthjee@gmail.com>
2025-07-24 19:47:26 -0300

    Update schema

--- source/db/schema.rb
+++ source/db/schema.rb
@@ -2,17 +2,32 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_125814) do
+ActiveRecord::Schema[7.2].define(version: 2023_07_22_014647) do
+  create_table "active_settings", charset: "utf8mb3", force: :cascade do |t|
+    t.string "key", limit: 50, null: false
+    t.string "value", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_active_settings_on_key", unique: true
+  end
 
-  create_table "simulation_contagion_behaviors", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "sessions", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "expiration", precision: nil
+    t.string "token", limit: 64, null: false
+    t.index ["token"], name: "index_sessions_on_token", unique: true
+    t.index ["user_id"], name: "fk_rails_758836b4f0"
+  end
+
+  create_table "simulation_contagion_behaviors", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.bigint "contagion_id", null: false
     t.integer "interactions", null: false
     t.float "contagion_risk", null: false
@@ -22,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_125814) do
     t.index ["reference", "contagion_id"], name: "contagion_behaviors_reference_contagion_id", unique: true
   end
 
-  create_table "simulation_contagion_groups", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "simulation_contagion_groups", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.bigint "contagion_id", null: false
     t.float "lethality_override"
@@ -35,14 +50,14 @@ ActiveRecord::Schema.define(version: 2021_09_02_125814) do
     t.index ["reference", "contagion_id"], name: "index_simulation_contagion_groups_on_reference_and_contagion_id", unique: true
   end
 
-  create_table "simulation_contagion_instants", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "simulation_contagion_instants", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "contagion_id", null: false
     t.integer "day", null: false
     t.string "status", default: "created", null: false
     t.index ["contagion_id", "day"], name: "index_simulation_contagion_instants_on_contagion_id_and_day", unique: true
   end
 
-  create_table "simulation_contagion_populations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "simulation_contagion_populations", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "instant_id", null: false
     t.integer "group_id", null: false
     t.integer "behavior_id", null: false
@@ -56,7 +71,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_125814) do
     t.index ["instant_id", "group_id", "state", "days"], name: "simulation_contagion_unique_keys", unique: true
   end
 
-  create_table "simulation_contagions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "simulation_contagions", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "simulation_id", null: false
     t.float "lethality", null: false
     t.integer "days_till_recovery", null: false
@@ -67,7 +82,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_125814) do
     t.index ["simulation_id"], name: "index_simulation_contagions_on_simulation_id", unique: true
   end
 
-  create_table "simulation_graph_plots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "simulation_graph_plots", charset: "utf8mb3", force: :cascade do |t|
     t.string "label", null: false
     t.string "metric", limit: 7, null: false
     t.bigint "graph_id", null: false
@@ -78,31 +93,45 @@ ActiveRecord::Schema.define(version: 2021_09_02_125814) do
     t.index ["simulation_id"], name: "fk_rails_37bab834ce"
   end
 
-  create_table "simulation_graphs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "simulation_graphs", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "title"
   end
 
-  create_table "simulations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "simulations", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "algorithm", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "status", default: "created", null: false
     t.boolean "checked", default: false, null: false
   end
 
-  create_table "simulations_tags", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "simulations_tags", id: false, charset: "utf8mb3", force: :cascade do |t|
     t.bigint "simulation_id", null: false
     t.bigint "tag_id", null: false
     t.index ["simulation_id", "tag_id"], name: "index_simulations_tags_on_simulation_id_and_tag_id", unique: true
   end
 
-  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "tags", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", limit: 50, null: false
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "login", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "salt", null: false
+    t.boolean "admin", default: false, null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["login"], name: "index_users_on_login", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
   add_foreign_key "simulation_contagion_behaviors", "simulation_contagions", column: "contagion_id"
   add_foreign_key "simulation_contagion_groups", "simulation_contagion_behaviors", column: "behavior_id"
   add_foreign_key "simulation_contagion_groups", "simulation_contagions", column: "contagion_id"
