2d90f2b3a6e1d22e3fa826f4c2a7ab46f9fe9c1d
darthjee <darthjee@gmail.com>
2025-07-28 12:35:05 -0300

    Configure magicka

--- source/config/initializers/magicka.rb
+++ source/config/initializers/magicka.rb
@@ -2,6 +2,12 @@
 
 Magicka::Form.with_element('Magicka::NgSelect', :ng_select)
 Magicka::Form.with_element('Magicka::NgTextarea', :textarea)
+Magicka::Form.with_element('Magicka::NgButton', :ng_button)
+Magicka::Form.with_element('Magicka::NgTagsForm', :tags)
+
 Magicka::Display.with_element('Magicka::NgSelectText', :ng_select)
 Magicka::Display.with_element('Magicka::Text', :textarea)
+Magicka::Display.with_element('Magicka::NgButton', :ng_button)
+Magicka::Display.with_element('Magicka::NgTags', :tags)
+
 Magicka::Display.with_element('Magicka::NgPagination', :pagination)
