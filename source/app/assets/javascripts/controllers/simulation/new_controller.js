d47ca042a40c17d8840ca2ed71e5c8f790be68da
darthjee <darthjee@gmail.com>
2025-08-08 21:26:04 -0300

    Revert "Remove duplicated method"
    This reverts commit acf41f9abdf8fb88dd93f6492104814ca3c2fa33.

--- source/app/assets/javascripts/controllers/simulation/new_controller.js
+++ source/app/assets/javascripts/controllers/simulation/new_controller.js
@@ -10,6 +10,10 @@
       };
     },
 
+    _goIndex: function() {
+      this.location.path(this.location.$$path.replace(/(\w*\/edit|new|\d+\/clone)/, ""));
+    },
+
     addObject: function(key) {
       if (!this.data.settings) {
         this.data.settings = {
@@ -73,9 +77,7 @@
   }
 
   app.controller("Simulation.NewController", [
-    "cyberhawk_builder", function(builder) {
-      builder.buildAndRequest(this, options);
-    }
+    "cyberhawk_builder", function(builder) { builder.buildAndRequest(this, options); }
   ]);
 }(window._, window.angular, window.Cyberhawk));
 
