d03e2c0a6539f72ac3843e01df402a2b49d22244
darthjee <darthjee@gmail.com>
2025-08-13 16:14:42 -0300

    Refactor worker

--- source/app/workers/simulation/processor_worker.rb
+++ source/app/workers/simulation/processor_worker.rb
@@ -4,21 +4,29 @@ class Simulation < ApplicationRecord
   class ProcessorWorker
     include Sidekiq::Worker
 
+    attr_reader :id
+
     def perform(id)
-      simulation = Simulation
-                   .eager_load(:contagion)
-                   .eager_load(contagion: :groups)
-                   .eager_load(contagion: :behaviors)
-                   .find(id)
+      @id = id
 
       Simulation::Processor.process(simulation, times: 1)
 
-      return if simulation.finished?
+      return if finished?
 
       next_worker.perform_async(id)
     end
 
     private
+
+    delegate :finished?, to: :simulation
+
+    def simulation
+      @simulation ||= Simulation
+                      .eager_load(:contagion)
+                      .eager_load(contagion: :groups)
+                      .eager_load(contagion: :behaviors)
+                      .find(id)
+    end
 
     def next_worker
       self.class
