d4507ce92c5a0c84d13198143db1e308a8ed8906
darthjee <darthjee@gmail.com>
2025-08-15 13:46:42 -0300

    Add base filter

---
+++ source/app/filters/base_filter.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class BaseFilter
+  class << self
+    def filter_by(attribute)
+      @filter_attributes ||= []
+      @filter_attributes << attribute
+    end
+
+    def filter_attributes
+      @filter_attributes || []
+    end
+  end
+
+  attr_reader :filters
+
+  def initialize(filters = {})
+    @filters = filters || {}
+  end
+
+  def apply(scope)
+    self.class.filter_attributes.each do |attribute|
+      filter_value = filters[attribute]
+      scope = scope.where(attribute => filter_value) if filter_value.present?
+    end
+    
+    scope
+  end
+end
--- source/app/filters/simulation/filter.rb
+++ source/app/filters/simulation/filter.rb
@@ -1,34 +1,8 @@
 # frozen_string_literal: true
 
 class Simulation < ApplicationRecord
-  class Filter
-    class << self
-      def filter_by(attribute)
-        @filter_attributes ||= []
-        @filter_attributes << attribute
-      end
-
-      def filter_attributes
-        @filter_attributes || []
-      end
-    end
-
-    attr_reader :filters
-
+  class Filter < ::BaseFilter
     filter_by :name
     filter_by :status
-
-    def initialize(filters = {})
-      @filters = filters || {}
-    end
-
-    def apply(scope)
-      self.class.filter_attributes.each do |attribute|
-        filter_value = filters[attribute]
-        scope = scope.where(attribute => filter_value) if filter_value.present?
-      end
-      
-      scope
-    end
   end
 end
