c99fa04274a3b247e5250a0abe0a4b24716122d5
darthjee <darthjee@gmail.com>
2025-08-19 15:04:31 -0300

    Add filter by tag

--- source/app/filters/simulation/filter.rb
+++ source/app/filters/simulation/filter.rb
@@ -4,5 +4,6 @@ class Simulation < ApplicationRecord
   class Filter < Kiroshi::Filters
     filter_by :name, match: :like
     filter_by :status
+    filter_by :tag_name, column: :name, table: :tags
   end
 end
