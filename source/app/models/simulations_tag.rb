223c60db2ea5f27da62360a98583df520a1c2aad
darthjee <darthjee@gmail.com>
2025-08-20 19:17:36 -0300

    Add query tag connection

--- source/app/models/simulations_tag.rb
+++ source/app/models/simulations_tag.rb
@@ -3,4 +3,7 @@
 class SimulationsTag < ApplicationRecord
   belongs_to :simulation
   belongs_to :tag
+  has_one :query_tag,
+          class_name: 'Tag', foreign_key: 'tag_id',
+          validate: false, autosave: false
 end
