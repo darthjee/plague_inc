af65fbc117dbec968fa2f8a6bd0a1131852280fc
darthjee <darthjee@gmail.com>
2025-08-20 19:27:10 -0300

    Fix connection

--- source/app/models/simulations_tag.rb
+++ source/app/models/simulations_tag.rb
@@ -4,6 +4,6 @@ class SimulationsTag < ApplicationRecord
   belongs_to :simulation
   belongs_to :tag
   has_one :query_tag,
-          class_name: 'Tag', foreign_key: 'tag_id',
+          class_name: 'Tag', primary_key: 'tag_id', foreign_key: 'id',
           validate: false, autosave: false
 end
